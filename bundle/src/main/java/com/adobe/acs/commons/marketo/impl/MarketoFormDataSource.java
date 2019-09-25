f86a600ec16276f370eb5169919da41bacd6383b
Dan Klco <dan.klco@perficient.com>
2019-09-25 12:03:34 -0400

    Adding JavaDocs and license headers

--- bundle/src/main/java/com/adobe/acs/commons/marketo/impl/MarketoFormDataSource.java
+++ bundle/src/main/java/com/adobe/acs/commons/marketo/impl/MarketoFormDataSource.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2019 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.marketo.impl;
 
 import java.util.ArrayList;
@@ -35,6 +54,9 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
+/**
+ * Used to drive the list of Form ID options for the Marketo Form component dialog.
+ */
 @Component(service = Servlet.class, property = { ServletResolverConstants.SLING_SERVLET_METHODS + "=GET",
     ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES
         + "=acs-commons/components/content/marketo-form/data-source" })
--- ui.apps/src/main/content/jcr_root/apps/acs-commons/components/content/marketo-form/marketo-form.jsp
+++ ui.apps/src/main/content/jcr_root/apps/acs-commons/components/content/marketo-form/marketo-form.jsp
@@ -1,3 +1,22 @@
+<%--
+  #%L
+  ACS AEM Commons Package
+  %%
+  Copyright (C) 2019 Adobe
+  %%
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+       http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  #L%
+  --%>
 <%@include file="/libs/foundation/global.jsp" %><%@taglib prefix="sling2" uri="http://sling.apache.org/taglibs/sling" %>
 <c:set var="mktoConfig" value="${sling2:adaptTo(slingRequest,'com.adobe.acs.commons.marketo.models.MarketoClientConfigurationManager').configuration}" />
 <sling2:adaptTo adaptable="${slingRequest}" adaptTo="com.adobe.acs.commons.marketo.models.MarketoFormCmp" var="formCmp" />
